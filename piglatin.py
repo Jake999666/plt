d81c22f0e9bcd8b2d6afa99162012f987d04093e
Jaakko <jmatikai21@student.oulu.fi>
2024-10-31 18:36:52 +0200

    us3, tests

--- piglatin.py
+++ piglatin.py
@@ -9,5 +9,14 @@ class PigLatin:
     def translate(self) -> str:
         if self.phrase == "":
             return "nil"
+        if self.phrase[0] in ["a", "e", "i", "o", "u"]:
+            if self.phrase[-1] == "y":
+                return self.phrase + "nay"
+            elif self.phrase[-1] in ["a", "e", "i", "o", "u"]:
+                return self.phrase + "yay"
+            else:
+                return self.phrase + "ay"
+
+
         return self.phrase
 
--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -1,4 +1,6 @@
 import unittest
+from tokenize import triple_quoted
+
 from piglatin import PigLatin
 from error import PigLatinError
 
@@ -16,6 +18,19 @@ class TestPigLatin(unittest.TestCase):
         translator = PigLatin(phrase)
         self.assertEqual(translator.translate(), "nil")
 
+    def test_starting_nay(self):
+        phrase = "any"
+        translator = PigLatin(phrase)
+        self.assertEqual(translator.translate(),"anynay")
 
+    def test_ending_yay(self):
+        phrase = "apple"
+        translator = PigLatin(phrase)
+        self.assertEqual(translator.translate(),"appleyay")
+
+    def test_ending_ay(self):
+        phrase = "ask"
+        translator = PigLatin(phrase)
+        self.assertEqual(translator.translate(),"askay")
 
 
